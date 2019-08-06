e4cb1ec46dc0be72c3ca35bbe37cca6634d87e09
Martyna <martyna.glaz@gmail.com>
2019-08-06 01:21:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const driversFirst = ['Sally', 'Bob']
+const returnFirstTwoDrivers = function(drivers){
+  return driversFirst
+}
+const driversLast = ['Freddy', 'Claudia']
+const returnLastTwoDrivers = function (drivers){
+  return driversLast;
+}
+selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+  return function (){
+    return (number * number);
+  };
+}
+const fareDoubler = function (number){return (number * 2);};
+const fareTripler = function (number){return (number * 3);};
+
+const fetchSpecifiedDrivers = function fetchSpecifiedDrivers(drivers, functi(){
+  return returnFirstTwoDrivers() ===  returnLastTwoDrivers()})
+    return true;
+  }
